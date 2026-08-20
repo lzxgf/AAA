297e95fa7df16b44fbacb87210a20a42ea9ec4ac
lzxgf <1690185297@qq.com>
2026-08-20 16:17:06 +0800

    feat:提交vite

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+export const PROJECT_KEYWORD_TYPE_OPTIONS = [
+  { label: '项目名称备注', value: 'project_name' },
+  { label: '项目ID', value: 'project_id' },
+  { label: '关联抖音号ID', value: 'aweme_id' },
+  { label: '专辑ID', value: 'playlet_album_id' }
+] as const
