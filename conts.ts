18d89ffde44b48fad0c8a36f98351200d1380d51
lzxgf <1690185297@qq.com>
2026-08-20 14:59:29 +0800

    feat:添加账户名称

---
+++ conts.ts
@@ -0,0 +1,7 @@
+export const PROJECT_KEYWORD_TYPE_OPTIONS = [
+  { label: '项目名称', value: 'project_name' },
+  { label: '账户名称', value: 'account_name' },
+  { label: '项目ID', value: 'project_id' },
+  { label: '关联抖音号ID', value: 'aweme_id' },
+  { label: '专辑ID', value: 'playlet_album_id' }
+] as const
