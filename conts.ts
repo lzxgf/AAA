0950dbc79bc9f9110fa45b056d4a24920dbae7d9
lzxgf <1690185297@qq.com>
2026-08-24 13:44:59 +0800

    修改conts文件

--- conts.ts
+++ conts.ts
@@ -1,5 +1,13 @@
+/*
+ * @Author: lzxgf 1690185297@qq.com
+ * @Date: 2026-08-20 14:57:24
+ * @LastEditors: lzxgf 1690185297@qq.com
+ * @LastEditTime: 2026-08-24 13:44:44
+ * @FilePath: \AAA\conts.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 export const PROJECT_KEYWORD_TYPE_OPTIONS = [
-  { label: '项目名称备注', value: 'project_name' },
+  { label: '项目名称', value: 'project_name' },
   { label: '项目ID', value: 'project_id' },
   { label: '关联抖音号ID', value: 'aweme_id' },
   { label: '专辑ID', value: 'playlet_album_id' }
